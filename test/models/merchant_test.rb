399feae266f0f7617e14f1576f0b88e76cea9117
Stephanie Marchante <smarcha04@gmail.com>
2019-05-01 16:27:35 -0700

    debuggin orderitem tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,14 +1,17 @@
 require "test_helper"
+require "pry"
 
 describe Merchant do
   let(:merchant) { Merchant.new }
   let(:merch_1) { merchants(:merch_1) }
-
-  it "must be valid" do
-    value(merchant).must_be :valid?
-  end
-
+ 
+ 
   describe "relations" do
+    it "must be valid" do
+      binding.pry
+      value(merchant).must_be :valid?
+    end
+  
     it "has products" do
       expect( merch_1.products ).must_respond_to :each
 
--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
 
+
 describe Orderitem do
   let(:orderitem) { Orderitem.new }
+  let(:orderitem_1) { orderitems(:orderitem_1) }
 
-  it "must be valid" do
-    value(orderitem).must_be :valid?
+  describe "valid" do
+    it "must be valid" do
+      value(orderitem).must_be :valid?
+    end
   end
 end
