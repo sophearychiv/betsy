de045b31292efd896d8087cccfca70a088d1efee
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-01 14:57:30 -0700

    updated reviews fixtures and tests to include a product_id attribute

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,11 +1,10 @@
 require "test_helper"
 
 describe Review do
-  let(:review) { Review.new }
   let(:review_one) { reviews(:review_one) }
 
   it "must be valid" do
-    value(review).must_be :valid?
+    expect(review_one).must_be :valid?
   end
 
   describe "relations" do
