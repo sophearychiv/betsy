d8d9153b060d12a18d0c83848eaa4514394ea936
Kate Sandler <kate@katekatekate.net>
2019-05-01 16:04:08 -0700

    Adds seeds for merchants and products

---
+++ db/generate_seeds.rb
@@ -0,0 +1,27 @@
+require 'faker'
+require 'csv'
+CSV.open("../db/merchant_seeds.csv", "w", :write_headers => true,
+  :headers => ["provider", "email", "username"]) do |csv|
+    100.times do
+        provider = %w(github google).sample
+        email = Faker::Internet.email
+        username = (Faker::Creature::Cat.name).downcase + rand(0..100).to_s
+        stock = rand(0..4)
+        description = Faker::Movies::StarWars.wookiee_sentence
+        active = true
+    csv << [provider, email, username]
+    end
+  end
+CSV.open("../db/product_seeds.csv", "w", :write_headers => true,
+  :headers => ["name", "price", "merchant_id", "stock", "description", "active", "photo"]) do |csv|
+    100.times do
+        name = Faker::Commerce.product_name
+        price = Faker::Commerce.price
+        merchant_id = Faker::Number.number(2)
+        stock = rand(0..4)
+        description = Faker::Movies::StarWars.wookiee_sentence
+        active = true
+        photo = "lorempixel.com/200/200/animals"
+    csv << [name, price, merchant_id, stock, description, active, photo]
+    end
+end
