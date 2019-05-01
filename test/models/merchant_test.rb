d82280bebe918c23c8b9233b4ef496ba9dc1c919
Stephanie Marchante <smarcha04@gmail.com>
2019-05-01 16:41:57 -0700

    updated merchants yml and test

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,21 +1,19 @@
 require "test_helper"
-require "pry"
 
 describe Merchant do
   let(:merchant) { Merchant.new }
-  let(:merch_1) { merchants(:merch_1) }
+  let(:kim) { merchants(:kim) }
  
  
   describe "relations" do
     it "must be valid" do
-      binding.pry
       value(merchant).must_be :valid?
     end
   
     it "has products" do
-      expect( merch_1.products ).must_respond_to :each
+      expect( kim.products ).must_respond_to :each
 
-      merch_1.products.each do |product|
+      kim.products.each do |product|
         expect( product ).must_be_instance_of Product
       end
     end  
