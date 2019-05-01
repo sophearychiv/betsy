3e158d650857ac778fa174d4b90f935894e3b628
Stephanie Marchante <smarcha04@gmail.com>
2019-05-01 16:46:54 -0700

    adjusting orderitem test

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -1,9 +1,6 @@
 require "test_helper"
 
-
 describe Orderitem do
-  let(:orderitem) { Orderitem.new }
-  let(:orderitem_1) { orderitems(:orderitem_1) }
 
   describe "valid" do
     it "must be valid" do
