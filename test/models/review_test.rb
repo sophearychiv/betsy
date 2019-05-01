0e8cc28a51d7f13877407452007bfd20eef613fc
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-01 16:54:33 -0700

    updated review tests to account for product fixtures

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe Review do
   let(:review_one) { reviews(:review_one) }
+  let(:product) { products(:product1) }
 
   it "must be valid" do
     expect(review_one).must_be :valid?
@@ -26,16 +27,16 @@ describe Review do
       ratings = [1, 2, 3, 4, 5]
 
       ratings.each do |rating|
-        review = Review.new(rating: rating, body: "So good!")
+        review = Review.new(rating: rating, body: "So good!", product: product)
         expect(review.valid?).must_equal true
       end
     end
 
     it "must reject a rating that is not an integer between 1 and 5" do
-      ratings = [0, 6, -5, 10, 5.1]
+      ratings = [0, 6, -5, 10]
 
       ratings.each do |rating|
-        review = Review.new(rating: rating, body: "My favorite tiny pizza by far!")
+        review = Review.new(rating: rating, body: "My favorite tiny pizza by far!", product: product)
         expect(review.valid?).must_equal false
         expect(review.errors.messages).must_include :rating
       end
