ffcd294d8513e19654c53e5daa061ffad2362307
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-01 17:00:19 -0700

    2nd test for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,11 +3,19 @@ require "test_helper"
 describe Order do
   let(:order) { orders(:one) }
 
+  it "must be valid" do
+    order = orders(:one)
+    valid_order = order.valid?
+    expect(valid_order).must_equal true
+  end
+
   describe "Validations" do
-    it "must be valid" do
-      order = orders(:one)
+    it "requires user's name" do
+      order.name = nil
       valid_order = order.valid?
-      expect(valid_order).must_equal true
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :name
+      expect(order.errors.messages[:name]).must_equal ["can't be blank"]
     end
   end
 end
