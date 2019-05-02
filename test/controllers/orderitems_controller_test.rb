6041f78157e6f73a2825073f62ffbfba35c2b94b
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-02 16:58:30 -0700

    deleted out default tests, which were not funcitonal

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,24 +1,23 @@
 require "test_helper"
 
 describe OrderitemsController do
-  it "should get create" do
-    get orderitems_create_url
-    value(response).must_be :success?
+  describe "create" do
+    it "can create a new order item" do
+    end
   end
 
-  it "should get edit" do
-    get orderitems_edit_url
-    value(response).must_be :success?
+  describe "edit" do
+    it "can edit an existing order item" do
+    end
   end
 
-  it "should get update" do
-    get orderitems_update_url
-    value(response).must_be :success?
+  describe "update" do
+    it "should get update" do
+    end
   end
 
-  it "should get destroy" do
-    get orderitems_destroy_url
-    value(response).must_be :success?
+  describe "destroy" do
+    it "should get destroy" do
+    end
   end
-
 end
