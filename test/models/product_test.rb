10fd21b1335422d0f5aa1555d360421229a59dee
Kate Sandler <kate@katekatekate.net>
2019-05-01 18:39:01 -0700

    Adds relationship tests for products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -23,4 +23,42 @@ describe Product do
   it "returns not yet rated if there are no ratings" do
     expect(products(:product3).average_rating).must_equal 'Not yet rated'
   end
+
+describe 'Relationships' do
+
+  it 'can have many reviews' do
+    reviews = product.reviews
+
+    expect(reviews.length).must_be :>=, 1
+
+    reviews.each do |review|
+      expect(review).must_be_instance_of Review
+    end
+  end
+
+  it 'can have many orders' do
+    orders = product.orders
+
+    expect(orders.length).must_be :>=, 
+    orders.each do |order|
+      expect(order).must_be_instance_of Order
+    end
+  end
+
+  it 'can have many order_items' do
+    Orderitem.create(order: orders(:one), product: products(:product1), quantity: 5)
+    order_items = product.orderitems
+
+    expect(order_items.length).must_be :>=, 2
+    order_items.each do |order_item|
+      expect(order_item).must_be_instance_of Orderitem
+    end
+  end
+
+  it 'belongs to a user' do
+    merchant = product.merchant
+    expect(merchant).must_be_instance_of Merchant
+    expect(merchant.id).must_equal product.merchant_id
+  end
+end
 end
