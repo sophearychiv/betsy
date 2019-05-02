6251b29d5672dc03a732c87fc7200640e4112c94
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-02 12:39:37 -0700

    changed realtionships in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  has_and_belongs_to_many :products
+  has_many :products, through: :orderitems
   validates :name, presence: true
   validates :email, presence: true
   validates :address, presence: true
