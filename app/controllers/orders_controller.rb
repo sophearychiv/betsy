86727219384b313659f2979c0cb582c6ef3f2431
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-02 16:06:48 -0700

    added orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    order = Order.new(order_params)
+    if order.save
+      flash[:success] = "Your payment has been process! Thank you for your business!"
+      redirect_to order_path(order.id)
+    else
+      order.errors.messages.each do |field, message|
+        flash.now[field] = messages
+      end
+
+      render :new, status: :bad_request
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def order_params
+    return params.require(:order).permit(:address, :name, :status, :cc, :expiration_date, :email, :csv)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "homepages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:new, :create]
 end
