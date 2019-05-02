57c0c0c984d2d7d4824195d3930d8a44a9376cca
Stephanie Marchante <smarcha04@gmail.com>
2019-05-02 15:08:23 -0700

    basic merchant controller actions

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -12,9 +12,31 @@ class MerchantsController < ApplicationController
   
   def create
     @merchant = Merchant.new(merchant_params)
+
   end
 
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    render :not_found unless @merchant
+  end
 
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+
+    if @merchant.save(merchant_params)
+      redirect_to merchant_path(@merchant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @merchant.destroy
+  end
 
 
   private 
