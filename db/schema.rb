2928de9027437322175c01663a55f1ce0aee6768
Stephanie Marchante <smarcha04@gmail.com>
2019-05-01 17:26:52 -0700

    weird schema problems

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2019_05_02_001804) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories_products", force: :cascade do |t|
