803c8907b8131c8ec879c1d65454954b86c8ca26
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-01 17:22:40 -0700

    all validation tests for order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -17,5 +17,45 @@ describe Order do
       expect(order.errors.messages).must_include :name
       expect(order.errors.messages[:name]).must_equal ["can't be blank"]
     end
+
+    it "requires an email" do
+      order.email = nil
+      valid_order = order.valid?
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :email
+      expect(order.errors.messages[:email]).must_equal ["can't be blank"]
+    end
+
+    it "requires an address" do
+      order.address = nil
+      valid_order = order.valid?
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :address
+      expect(order.errors.messages[:address]).must_equal ["can't be blank"]
+    end
+
+    it "requires cc" do
+      order.cc = nil
+      valid_order = order.valid?
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :cc
+      expect(order.errors.messages[:cc]).must_equal ["can't be blank"]
+    end
+
+    it "requires csv" do
+      order.csv = nil
+      valid_order = order.valid?
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :csv
+      expect(order.errors.messages[:csv]).must_equal ["can't be blank"]
+    end
+
+    it "requires expiration_date" do
+      order.expiration_date = nil
+      valid_order = order.valid?
+      expect(valid_order).must_equal false
+      expect(order.errors.messages).must_include :expiration_date
+      expect(order.errors.messages[:expiration_date]).must_equal ["can't be blank"]
+    end
   end
 end
