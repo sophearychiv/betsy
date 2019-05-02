90e47ea772ae659be899ba3f8cb4fd7f763ed302
Stephanie Marchante <smarcha04@gmail.com>
2019-05-02 14:20:50 -0700

    finished validation tests for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -13,22 +13,58 @@ describe Merchant do
 
   
     it "has products" do
-      expect( @merch.products ).must_respond_to :each
+      expect(@merch.products).must_respond_to :each
 
       @merch.products.each do |product|
-        expect( product ).must_be_instance_of Product
+        expect(product).must_be_instance_of Product
       end
     end  
     
   end
 
-  # describe "validations" do
-  #   let(:new_merchant) {
-  #     Merchant.new
-  #   }
-  #   it "" do
+  describe "validations" do
+    let(:new_merch) {
+      Merchant.new(
+        username: 'testuser',
+        email: 'testemail@itsy.com',
+        uid: 1010,
+        provider: 'github'
+      )
+    }
+    it "is a valid with unique email and username" do
+      
+      expect(@merch).must_be :valid?
 
-    
-  #   end
-  # end
+    end
+
+    it "invalid without username" do
+      @merch.username = nil
+
+      expect(@merch).wont_be :valid?
+    end 
+
+    it "invalid with already used username" do
+      expect(new_merch).must_be :valid?
+
+      new_merch.username = @merch.username
+
+      expect(new_merch).wont_be :valid?
+
+    end
+
+    it "invalid without an email" do
+      new_merch.email = nil
+      
+      expect(new_merch).wont_be :valid?
+    end 
+
+    it "invalid with already used email" do
+      expect(new_merch).must_be :valid?
+  
+      new_merch.email = @merch.email
+
+      expect(new_merch).wont_be :valid?
+
+    end
+  end
 end
