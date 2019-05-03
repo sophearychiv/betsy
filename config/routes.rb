12f4f5a049d9c3b6d7e3e20a714f87973fb930a5
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-02 17:28:58 -0700

    updated routes for reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   root "homepages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orderitems, except: [:index, :new, :show]
-  resources :products, except: [:destroy]
+  resources :products, except: [:destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+
   patch "/products/:id/retire", to: "products#retire", as: "retire"
 
   resources :orders, only: [:new, :create]
