c807870ab7287ad4a0ea90001cb66c5fc6c895fa
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-03 15:10:16 -0700

    tests for orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,14 @@ Rails.application.routes.draw do
   resources :products, except: [:destroy]
   patch "/products/:id/retire", to: "products#retire", as: "retire"
 
-  resources :orders, only: [:new, :create]
+  resources :orders, only: [:new, :create, :show, :index]
   get "/orders/confirmation", as: "confirmation"
   # post "/orders/checkout", to: "orders#checkout"
   resources :sessions, only: [:new, :create]
   resources :merchants, except: [:new, :create]
 
-
   get "/auth/github", as: "github_login"
-  get '/auth/:provider/callback', to: "sessions#create", as: 'auth_callback'
+  get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
 
-
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  delete "/logout", to: "sessions#destroy", as: "logout"
 end
