9caf0d3463ab60fc1c78e425fab848d38d1ec0a0
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-03 02:56:37 -0700

    added tests for review controller new action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,30 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @product = Product.find_by(id: params[:product_id])
+
+    if @product
+      @review = Review.new(review_params)
+      if @review.save
+        flash[:success] = "Thank you for your feedback"
+        redirect_to product_path(params[:product_id])
+      else
+        flash[:error] = "An itsty problem occurred: Could not process feedback"
+        @review.errors.messages.each do |field, messages|
+          flash.now[field] = messages
+        end
+        render :new, status: :bad_request
+      end
+    else
+      flash[:error] = "An itsy problem occurred: Product not found"
+      redirect_to products_path
+    end
+  end
+
+  def review_params
+    return params.require(:review).permit(:rating, :body)
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,14 +1,16 @@
 require "test_helper"
 
 describe ReviewsController do
-  it "should get new" do
-    get reviews_new_url
-    value(response).must_be :success?
+  let(:product) { products(:product1) }
+
+  describe "new" do
+    it "should get the new review form for an existing product" do
+      get new_product_review_path(product.id)
+
+      must_respond_with :success
+    end
   end
 
   it "should get create" do
-    get reviews_create_url
-    value(response).must_be :success?
   end
-
 end
