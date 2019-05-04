ba775670f720bdc23af91eedfe7bcac8519a8de1
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-04 15:51:21 -0700

    updated create action for orderitems

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -5,33 +5,38 @@ class OrderitemsController < ApplicationController
   # end
 
   def create
+    # raise
     @product = Product.find_by(id: params[:product_id])
 
     if @product
       # product_id, order_id, and quantity
-      @orderitem = Orderitem.new(orderitems_params)
-      @product.stock -= params[:quantity]
+      # @orderitem = Orderitem.new(orderitem_params)
+      @orderitem = Orderitem.new(quantity: params[:quantity].to_i)
+      @product.stock -= params[:quantity].to_i
 
       if !@product.valid?
-        flash[:error] = "An itsy problem occurred"
+        flash[:status] = :warning
+        flash[:result_text] = "An itsy problem occurred"
         @product.errors.messages.each do |field, messages|
           flash.now[field] = messages
         end
-        redirect_to products_path(params[:product_id])
+        redirect_to product_path(params[:product_id])
+        return
       end
 
       if !session[:order_id]
-        order = Order.create
-        session[:order_id] = order.id
+        @order = Order.create
+        session[:order_id] = @order.id
       end
-
-      @order_item.product_id = @product.id
-      @order_item.order_id = session[:order_id]
-
-      flash[:success] = "Succesfully added an itsy item to your cart"
+      @orderitem.product_id = @product.id
+      @orderitem.order_id = session[:order_id]
+      @orderitem.save
+      flash[:status] = :success
+      flash[:result_text] = "Succesfully added an itsy item to your cart"
       redirect_to orders_path
     else
-      flash[:error] = "An itsy problem occurred: can't find product"
+      flash[:status] = :warning
+      flash[:result_text] = "An itsy problem occurred: can't find product"
       redirect_to products_path
     end
   end
@@ -47,7 +52,7 @@ class OrderitemsController < ApplicationController
 
   private
 
-  def orderitem_params
-    return params.require(:orderitem).permit(:quantity)
-  end
+  # def orderitem_params
+  #   return params.require(:orderitem).permit(:quantity)
+  # end
 end
