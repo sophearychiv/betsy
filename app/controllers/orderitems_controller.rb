0cdf8c9e16a68ef355c16a0c34d5fe7108c39dc8
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-04 12:52:08 -0700

    added create method to orderitems

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,5 +1,34 @@
 class OrderitemsController < ApplicationController
   def create
+    @product = Product.find_by(id: params[:product_id])
+
+    if @product
+      # product_id, order_id, and quantity
+      @orderitem = Orderitem.new(orderitems_params)
+      @product.stock -= params[:quantity]
+
+      if !@product.valid?
+        flash[:error] = "An itsy problem occurred"
+        @product.errors.messages.each do |field, messages|
+          flash.now[field] = messages
+        end
+        redirect_to products_path(params[:product_id])
+      end
+
+      if !session[:order_id]
+        order = Order.create
+        session[:order_id] = order.id
+      end
+
+      @order_item.product_id = @product.id
+      @order_item.order_id = session[:order_id]
+
+      flash[:success] = "Succesfully added an itsy item to your cart"
+      redirect_to orders_path
+    else
+      flash[:error] = "An itsy problem occurred: can't find product"
+      redirect_to products_path
+    end
   end
 
   def edit
@@ -9,5 +38,11 @@ class OrderitemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def orderitem_params
+    return params.require(:orderitem).permit(:quantity)
   end
 end
