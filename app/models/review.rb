7ca3ad8daf4f74408467ff77ca070cc289b794c5
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-03 21:29:13 -0700

    changed error message for an invalid rating value and updated tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, presence: true,
-                     :inclusion => 1..5
+                     :inclusion => { :in => 1..5,
+                                    :message => "must be a number between 1 and 5" }
   validates :body, presence: true
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -5,7 +5,7 @@ describe Review do
   let(:product) { products(:product1) }
 
   it "must be valid" do
-    expect(review_one).must_be :valid?
+    expect(review_one.valid?).must_equal true
   end
 
   describe "relations" do
@@ -21,6 +21,7 @@ describe Review do
 
       expect(review.valid?).must_equal false
       expect(review.errors.messages).must_include :rating
+      expect(review.errors.messages[:rating]).must_include "can't be blank"
     end
 
     it "accepts an integer rating between 1 and 5" do
@@ -39,6 +40,7 @@ describe Review do
         review = Review.new(rating: rating, body: "My favorite tiny pizza by far!", product: product)
         expect(review.valid?).must_equal false
         expect(review.errors.messages).must_include :rating
+        expect(review.errors.messages[:rating]).must_equal ["must be a number between 1 and 5"]
       end
     end
 
@@ -47,6 +49,7 @@ describe Review do
 
       expect(review.valid?).must_equal false
       expect(review.errors.messages).must_include :body
+      expect(review.errors.messages[:body]).must_equal ["can't be blank"]
     end
   end
 end
