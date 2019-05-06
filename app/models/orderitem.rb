73be83a3f547d4a89b43c21246417f151fa653c5
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-05 22:34:49 -0700

    created a custom method quantity_difference that we may not need

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -1,6 +1,10 @@
 class Orderitem < ApplicationRecord
-  belongs_to :order 
+  belongs_to :order
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
+
+# def quantity_difference
+#   return @orderitem.quantity - session[:quantity].to_i
+# end
