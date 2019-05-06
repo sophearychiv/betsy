75475bc4e1e512376c2f9e811ef4832a11597e71
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-05 22:55:25 -0700

    updated tests for orderitem model

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -1,49 +1,47 @@
 require "test_helper"
 
 describe Orderitem do
-  before do
-    @orderitem = orderitems(:item1)
-  end
+  let(:orderitem) { orderitems(:item1) }
 
   it "must be valid" do
-    value(@orderitem).must_be :valid?
+    value(orderitem).must_be :valid?
   end
 
   describe "relationships" do
     it "has an order" do
-      @orderitem.must_respond_to :order
-      @orderitem.order.must_be_kind_of Order  
+      orderitem.must_respond_to :order
+      orderitem.order.must_be_kind_of Order
     end
 
     it "has a product" do
-      @orderitem.must_respond_to :product
-      @orderitem.product.must_be_kind_of Product
-    end
-
-  end 
-
-  describe "validations" do  
-    it "valid with quantity" do
-      
-      expect(@orderitem).must_be :valid?
-
-    end
-
-    it "invalid without quantity" do
-      @orderitem.quantity = nil
-
-      expect(@orderitem).wont_be :valid?
-    end
-
-    it "invalid when the quantity is less than zero" do
-      @orderitem.quantity = -1
-
-      expect(@orderitem).wont_be :valid?
-
-      @orderitem.quantity = 0
-
-      expect(@orderitem).wont_be :valid?
+      orderitem.must_respond_to :product
+      orderitem.product.must_be_kind_of Product
     end
   end
 
+  describe "validations" do
+    it "valid with quantity" do
+      expect(orderitem).must_be :valid?
+    end
+
+    it "invalid without quantity" do
+      orderitem.quantity = nil
+
+      expect(orderitem).wont_be :valid?
+      expect(orderitem.errors.messages).must_include :quantity
+      expect(orderitem.errors.messages[:quantity]).must_equal ["can't be blank", "is not a number"]
+    end
+
+    it "invalid when the quantity is less than or equal to zero or nil" do
+      quantities = [-1, 0]
+
+      quantities.each do |quantity|
+        orderitem.quantity = quantity
+
+        expect(orderitem).wont_be :valid?
+        expect(orderitem.errors.messages).must_include :quantity
+        expect(orderitem.errors.messages[:quantity]).must_equal ["must be greater than 0"]
+      end
+    end
+  end
 end
