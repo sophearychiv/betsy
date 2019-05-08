206aec7308af4265356932cad1162bfb0452c26d
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-08 16:04:41 -0700

    completed seed file and csv for products updates

--- db/generate_seeds.rb
+++ db/generate_seeds.rb
@@ -9,16 +9,3 @@ CSV.open("../db/merchant_seeds.csv", "w", :write_headers => true,
     csv << [provider, email, username]
   end
 end
-# CSV.open("../db/product_seeds.csv", "w", :write_headers => true,
-#   :headers => ["name", "price", "merchant_id", "stock", "description", "active", "photo_url"]) do |csv|
-#     100.times do
-#         name = Faker::Commerce.product_name
-#         price = rand(0..9999)
-#         merchant_id = Faker::Number.number(2)
-#         stock = rand(0..99)
-#         description = Faker::Movies::StarWars.wookiee_sentence
-#         active = true
-#         photo_url = "lorempixel.com/200/200/animals"
-#     csv << [name, price, merchant_id, stock, description, active, photo_url]
-#     end
-# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "Loading raw merchant data from #{MERCHANTS_FILE}"
 
 category_failures = []
 
-categories = ["Bitty Bevs", "Tiny Treats", "Pint-Sized Plants", "Puny Pets", "Teeny Tech", "Bitsy Bags", "Little Library"]
+categories = ["Bitty Bevs", "Tiny Treats", "Finger Foods", "Bitsy Baubles", "Pint-Sized Plants", "Teeny Tech", "Mini Miscellanies"]
 categories.each do |category|
   category_new = Category.new(name: category)
 
@@ -62,3 +62,31 @@ end
 
 puts "Added #{Product.count} product records"
 puts "#{product_failures.length} product failed to save"
+
+Product.where(id: 1..5).each do |product|
+  product.categories << Category.find_by(name: "Bitty Bevs")
+end
+
+Product.where(id: 6..14).each do |product|
+  product.categories << Category.find_by(name: "Tiny Treats")
+end
+
+Product.where(id: 15..21).each do |product|
+  product.categories << Category.find_by(name: "Finger Foods")
+end
+
+Product.where(id: 22..24).each do |product|
+  product.categories << Category.find_by(name: "Bitsy Baubles")
+end
+
+Product.where(id: 25..27).each do |product|
+  product.categories << Category.find_by(name: "Pint-Sized Plants")
+end
+
+Product.where(id: 28..31).each do |product|
+  product.categories << Category.find_by(name: "Teeny Tech")
+end
+
+Product.where(id: 32..37).each do |product|
+  product.categories << Category.find_by(name: "Mini Miscellanies")
+end
