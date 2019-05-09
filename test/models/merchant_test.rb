88b9d1152418e7afc27a1db976acb0d5da7319de
Kate Sandler <kate@katekatekate.net>
2019-05-09 14:44:56 -0700

    Making way through merge

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe Merchant do
-  before do 
+  before do
     @merch =  merchants(:kim)
     @merch2 = merchants(:stephanie)
   end
@@ -10,16 +10,12 @@ describe Merchant do
     it "must be valid" do
       value(@merch).must_be :valid?
     end
-
-  
     it "has products" do
       expect(@merch.products).must_respond_to :each
-
       @merch.products.each do |product|
         expect(product).must_be_instance_of Product
       end
-    end  
-    
+    end
   end
 
   describe "validations" do
@@ -32,46 +28,37 @@ describe Merchant do
       )
     }
     it "is a valid with unique email and username" do
-      
       expect(@merch).must_be :valid?
-
     end
 
     it "invalid without username" do
       @merch.username = nil
-
       expect(@merch).wont_be :valid?
-    end 
+    end
 
     it "invalid with already used username" do
       expect(new_merch).must_be :valid?
-
       new_merch.username = @merch.username
-
       expect(new_merch).wont_be :valid?
 
     end
 
     it "invalid without an email" do
       new_merch.email = nil
-      
       expect(new_merch).wont_be :valid?
-    end 
+    end
 
     it "invalid with already used email" do
       expect(new_merch).must_be :valid?
-  
       new_merch.email = @merch.email
-
       expect(new_merch).wont_be :valid?
-
     end
 
   end
 
   describe "custom methods" do
     describe "total revenue" do
-      before do 
+      before do
         @merch = merchants(:sopheary)
       end
 
@@ -85,9 +72,9 @@ describe Merchant do
         order1 = orderitems(:item5)
       end
 
-      it "returns correct number of orders for current merchant" do  
+      it "returns correct number of orders for current merchant" do
         @merch2.orderitems.length.must_equal 3
-      end  
+      end
 
       it "returns the correct sum for paid orders for current merchant" do
         expect(@merch2.paid_orders_sum).must_equal 1197
@@ -96,8 +83,7 @@ describe Merchant do
 
     describe "complete orders sum" do
       it "returns the correct sum for complete orders for current merchant" do
-        
-      end 
+      end
     end
   end
 end
