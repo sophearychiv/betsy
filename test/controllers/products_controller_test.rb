4a08a6201c0cfbaded8490a7a6755c40f2cbae92
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-09 10:01:41 -0700

    more tests for products controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,13 +5,21 @@ describe ProductsController do
   describe "new" do
     it "can get the new product page" do
       get new_product_path
+      must_respond_with :success
+    end
+  end
+
+  describe "index" do
+    it "can get the index page" do
+      get products_path
+      must_respond_with :success
     end
   end
 
   describe "retire" do
     it "can retire a product" do
       id = product.id
-      expect {patch retire_path(id)}.wont_change 'Product.count'
+      expect { patch retire_path(id) }.wont_change "Product.count"
       product.reload
       must_respond_with :redirect
       must_redirect_to products_path
