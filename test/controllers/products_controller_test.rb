be624d1a299bd9bb53e6587c412fdbbaac37ad52
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-09 14:43:14 -0700

    Revert "more tests for products controller"
    This reverts commit 4a08a6201c0cfbaded8490a7a6755c40f2cbae92.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,14 +5,6 @@ describe ProductsController do
   describe "new" do
     it "can get the new product page" do
       get new_product_path
-      must_respond_with :success
-    end
-  end
-
-  describe "index" do
-    it "can get the index page" do
-      get products_path
-      must_respond_with :success
     end
   end
 
@@ -24,7 +16,7 @@ describe ProductsController do
   describe "retire" do
     it "can retire a product" do
       id = product.id
-      expect { patch retire_path(id) }.wont_change "Product.count"
+      expect {patch retire_path(id)}.wont_change 'Product.count'
       product.reload
       must_respond_with :redirect
       must_redirect_to dashboard_path
