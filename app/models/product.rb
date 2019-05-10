95bb180a9b31d019d84f3f7958263f43b607b102
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-09 23:58:21 -0700

    increased model products tests to 100%

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,27 +8,10 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :merchant, message: "Merchant's products must have unique names " }
   validates :price, presence: true, numericality: { greater_than: 0, message: "Price must be greater than 0" }
   validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0, message: "Stock cannot go below 0" }
-  # validates :category, presence: true
 
   def self.active_products
     return Product.where(active: :true)
   end
-
-  # def self.sold(orderitems)
-  #   orderitems.each do |item|
-  #     item.product.stock -= item.stock
-  #     item.product.save
-  #   end
-  # end
-
-  # def self.in_stock?(orderitems)
-  #   orderitems.each do |item|
-  #     if item.product.stock < item.quantity
-  #       return false
-  #     end
-  #   end
-  #   return true
-  # end
 
   def average_rating
     sum = 0
@@ -44,12 +27,4 @@ class Product < ApplicationRecord
       return (sum * 1.0 / count).round(2)
     end
   end
-
-  # def self.merchant_list(id)
-  #   self.active_products.select { |prod| prod.merchant.id == id }
-  # end
-
-  # def self.category_list(id)
-  #   self.active_products.select { |prod| prod.category.id == id }
-  # end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,27 +6,14 @@ describe Product do
   it "must be valid" do
     validity = product.valid?
     expect(validity).must_equal true
-    end
+  end
 
-  it 'has required fields' do
+  it "has required fields" do
     fields = %i(name price merchant_id stock description active photo_url)
 
     fields.each do |field|
       expect(product).must_respond_to field
     end
-  end
-
-  it "returns its average rating" do
-    expect(product.average_rating).must_equal 5
-  end
-
-  it "returns not yet rated if there are no ratings" do
-    expect(products(:product3).average_rating).must_equal 0
-  end
-
-  it "can list just the active products" do
-    expect(Product.active_products.count).must_equal 3
-    expect(Product.active_products).wont_include products(:product4)
   end
 
   it "can list all of the inactive products" do
@@ -36,78 +23,69 @@ describe Product do
     end
   end
 
-describe 'Relationships' do
+  describe "Relationships" do
+    it "can have many reviews" do
+      reviews = product.reviews
+      expect(reviews.length).must_be :>=, 1
 
-  it 'can have many reviews' do
-    reviews = product.reviews
-    expect(reviews.length).must_be :>=, 1
+      reviews.each do |review|
+        expect(review).must_be_instance_of Review
+      end
+    end
 
-    reviews.each do |review|
-      expect(review).must_be_instance_of Review
+    it "can have many orders" do
+      orders = product.orders
+
+      expect(orders.length).must_be :>=, 1
+      orders.each do |order|
+        expect(order).must_be_instance_of Order
+      end
+    end
+
+    it "can have many order_items" do
+      Orderitem.create(order: orders(:one), product: products(:product1), quantity: 5)
+      order_items = product.orderitems
+
+      expect(order_items.length).must_be :>=, 2
+      order_items.each do |order_item|
+        expect(order_item).must_be_instance_of Orderitem
+      end
+    end
+
+    it "belongs to a user" do
+      merchant = product.merchant
+      expect(merchant).must_be_instance_of Merchant
+      expect(merchant.id).must_equal product.merchant_id
+    end
+
+    it "can have 1 or more categories by shovelling a category into product.categories" do
+      category = categories(:food)
+      product = products(:product1)
+      product.categories << category
+
+      expect(category.products).must_include product
+    end
+
+    it "can have 0 categories" do
+      categories = product.categories
+      expect(categories.length).must_equal 0
     end
   end
 
-  it 'can have many orders' do
-    orders = product.orders
+  describe "custom methods" do
+    let(:product) { products(:product1) }
 
-    expect(orders.length).must_be :>=, 1
-    orders.each do |order|
-      expect(order).must_be_instance_of Order
+    it "can list just the active products" do
+      expect(Product.active_products.count).must_equal 3
+      expect(Product.active_products).wont_include products(:product4)
+    end
+
+    it "returns its average rating" do
+      expect(product.average_rating).must_equal 5
+    end
+
+    it "returns not yet rated if there are no ratings" do
+      expect(products(:product3).average_rating).must_equal 0
     end
   end
-
-  it 'can have many order_items' do
-    Orderitem.create(order: orders(:one), product: products(:product1), quantity: 5)
-    order_items = product.orderitems
-
-    expect(order_items.length).must_be :>=, 2
-    order_items.each do |order_item|
-      expect(order_item).must_be_instance_of Orderitem
-    end
-  end
-
-  it 'belongs to a user' do
-    merchant = product.merchant
-    expect(merchant).must_be_instance_of Merchant
-    expect(merchant.id).must_equal product.merchant_id
-  end
-
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #
-# ^^^^ NEEDS TESTS FOR CATEGORIES RELATIONSHIP THINGS! ^^^^ #
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #
-end
-
-describe 'custom methods' do
-  let(:product) { products(:product2) }
-
-  # it 'can subtract from inventory if there is enough stock' do
-  #   inventory = product.stock
-  #   product.sold(1)
-  #   expect(product.stock).must_equal inventory - 1
-  # end
-
-  # it 'will not reduce stock if there is not enough' do
-  #   product.sold(20)
-  #   valid = product.save
-  #   expect(valid).must_equal false
-  # end
-
-  # it 'returns true if inventory is available to fill order' do
-  #   product = products(:product2)
-  #   quantity = product.stock - 1
-  #   expect(result = product.in_stock?(quantity)).must_equal true
-  # end
-
-  # it 'returns false if stock is insufficient to fill order' do
-  #   product = products(:product2)
-  #   quantity = product.stock + 1
-  #   expect(product.in_stock?(quantity)).must_equal false
-
-  # end
-  # it "returns true if stock is equal to requested quantity" do
-  #   product = products(:product2)
-  #   quantity = product.stock
-  #   expect(product.in_stock?(quantity)).must_equal true
-  # end
-end
 end
